f531699eab7f1530d56777acfdbb5970a0f89b1b
tech1savvy <amankumar010604@gmail.com>
2025-06-14 17:29:05 +0530

    update nixos to 25.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -29,7 +29,7 @@
       modules = [
         ./configuration.nix
         home-manager.nixosModules.default
-        nixos-hardware.nixosModules.lenovo-legion-16ach6h-hybrid
+        nixos-hardware.nixosModules.lenovo-legion-16ach6h
         spicetify-nix.nixosModules.default
       ];
     };
