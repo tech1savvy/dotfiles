452928c3bb09fa1019810e02d00b967d7f6407bf
tech1savvy <amankumar010604@gmail.com>
2025-06-14 17:29:32 +0530

    added moduler kickstart.nvim

---
+++ config/nvim/lua/custom/plugins/gruvbox.lua
@@ -0,0 +1,24 @@
+return {
+  { -- Easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'ellisonleao/gruvbox.nvim',  -- Change to gruvbox.nvim
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      -- Optional: setup gruvbox with custom options
+      require('gruvbox').setup {
+        -- Example: disable italic comments
+        italic = {
+          comments = false,
+        },
+        -- Other options can be set here, see the plugin README for more
+      }
+
+      -- Load the colorscheme
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
