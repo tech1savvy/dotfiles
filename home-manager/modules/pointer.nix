5481fbdbc2bcb754b2021ac43be7d5cc2ea2d4ae
tech1savvy <amankumar010604@gmail.com>
2025-07-19 22:13:12 +0530

    added pointers configuration in home.nix

---
+++ home-manager/modules/pointer.nix
@@ -0,0 +1,9 @@
+{
+  home.pointerCursor = {
+    name = "Breeze";
+    package = pkgs.kdePackages.breeze;
+    size = 24; # Set your desired size
+    gtk.enable = true;
+    x11.enable = true;
+  };
+}
