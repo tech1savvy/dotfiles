4e4fef2e4893b8cef70e282e59f838b868e00ec4
tech1savvy <amankumar010604@gmail.com>
2025-10-19 11:11:45 +0530

    added tmux config handled by home manager

--- home-manager/modules/tmux/default.nix
+++ home-manager/modules/tmux/default.nix
@@ -1,20 +1,22 @@
-{lib, pkgs, ...}:
-
-let
-    tmuxConf = lib.readFile ./tmux.conf;
-in
 {
+  lib,
+  pkgs,
+  ...
+}: let
+  tmuxConf = lib.readFile ./tmux.conf;
+in {
   programs.tmux = {
     enable = true;
     keyMode = "vi";
     # prefix = "C-a";
-    
+
     extraConfig = tmuxConf;
 
     plugins = with pkgs; [
-      tmuxPlugins.vim-tmux-navigator
-      tmuxPlugins.sensible
-      tmuxPlugins.tokyo-night-tmux
+      # tmuxPlugins.sensible # adds sensible defaults
+      tmuxPlugins.tmux-which-key # not working
+      tmuxPlugins.resurrect
+      tmuxPlugins.continuum
     ];
   };
 }
