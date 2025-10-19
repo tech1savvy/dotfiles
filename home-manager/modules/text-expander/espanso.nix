05f40ad7aae25046f1113ba121673637f6f5acd1
tech1savvy <amankumar010604@gmail.com>
2025-10-19 11:11:27 +0530

    updated espanso home manager module

--- home-manager/modules/text-expander/espanso.nix
+++ home-manager/modules/text-expander/espanso.nix
@@ -1,6 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   services.espanso = {
     enable = true;
     package = pkgs.espanso-wayland;
+  };
+
+  home.file = {
+    ".config/espanso/match/base.yml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/espanso/base.yml";
+    };
   };
 }
