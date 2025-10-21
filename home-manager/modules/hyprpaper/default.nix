93c8c0190b8454eac5855b0765310c4237749006
tech1savvy <amankumar010604@gmail.com>
2025-10-21 12:45:33 +0530

    add: hyprpaper home module

---
+++ home-manager/modules/hyprpaper/default.nix
@@ -0,0 +1,19 @@
+{
+  services.hyprpaper = {
+    enable = true;
+
+    settings = {
+      ipc = "off";
+      splash = false;
+      splash_offset = 2.0;
+
+      preload = [ 
+        "~/Wallpapers/flower.jpg"
+      ];
+
+      wallpaper = [
+        "~/Wallpapers/flower.jpg"
+      ];
+    };
+  };
+}
