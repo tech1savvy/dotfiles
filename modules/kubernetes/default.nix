11ca9377c3edd60fb5cf3a5f97785563bdad9a34
tech1savvy <amankumar010604@gmail.com>
2025-10-21 21:45:19 +0530

    add: kubernetes, prometheus and grafana modules

---
+++ modules/kubernetes/default.nix
@@ -0,0 +1,9 @@
+{
+  services.kubernetes = {
+    roles = [
+      "master"
+      "node"
+    ];
+    masterAddress = "127.0.0.1"; # localhost since, both master and node are on the same machine
+  };
+}
