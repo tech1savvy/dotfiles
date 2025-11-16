52521dcdeb1916d64715daa87d94214caee79f8f
tech1savvy <amankumar010604@gmail.com>
2025-11-16 22:26:33 +0530

    chore: remove unused terminal tools

--- modules/development/terminal.nix
+++ modules/development/terminal.nix
@@ -4,14 +4,9 @@
     wget # download files from the web
 
 
-    bat # quick file reader
-    lazygit # source: https://www.youtube.com/shorts/gim-riPYhVA
     fzf # source: https://www.youtube.com/shorts/iUHfq_JN4G8
 
-    tmux # terminal multiplier
 
-    # terminal-vim based file manager
-    yazi
 
     # json parser
     jq
@@ -19,15 +14,12 @@
     # comman command line tools bundle
     busybox # tree,
 
-    zoxide # smart cd tool
 
 
     bonk # combines touch and mkdir
 
     # modern alternatives
     duf # df
-    eza # ls
-    bat-extras.batman
     fastfetch # neofetch
     curlie # curl
 
