88f1f40e30d15275162ee019a680ed102c1f175d
tech1savvy <amankumar010604@gmail.com>
2025-11-16 22:29:31 +0530

    feat(stylix.nix): change cursor size and name

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,13 +1,12 @@
-{pkgs, ...}:
-{
+{pkgs, ...}: {
   stylix.enable = true;
 
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
 
   stylix.cursor = {
     package = pkgs.capitaine-cursors;
-    name = "Capitaine-Cursors";
-    size = 22;
+    name = "capitaine-cursors";
+    size = 20;
   };
 
   stylix.polarity = "dark";
