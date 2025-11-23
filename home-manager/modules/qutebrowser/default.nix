692936f69fd102f0dc49c6e5dcc1416783741661
tech1savvy <amankumar010604@gmail.com>
2025-11-23 13:16:27 +0530

    added gpt as search engine to qutebrowser

--- home-manager/modules/qutebrowser/default.nix
+++ home-manager/modules/qutebrowser/default.nix
@@ -12,6 +12,7 @@
       np = "https://search.nixos.org/packages?channel=25.05&query={}";
       no = "https://search.nixos.org/options?channel=25.05&query={}";
       p = "https://www.perplexity.ai/search?q={}";
+      gpt = "https://chatgpt.com/?q={}&hints=search";
       hm = "https://home-manager-options.extranix.com/?query={}+&release=release-25.05";
     };
 
