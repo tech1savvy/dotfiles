57d38a288608ba7bd2464c222530d5514ff5f7b8
tech1savvy <amankumar010604@gmail.com>
2025-12-03 08:15:11 +0530

    feat(nvim): add todo-comments snippets support

---
+++ config/nvim/lua/snippets/all.lua
@@ -0,0 +1,15 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+ls.add_snippets("all", {
+  s("todo", {
+    t("TODO: "),
+    i(1), -- Cursor jumps here after expansion
+  }),
+  -- Extend for others
+  s("bug", { t("BUG: "), i(1) }),
+  s("warning", { t("WARN: "), i(1) }),
+})
