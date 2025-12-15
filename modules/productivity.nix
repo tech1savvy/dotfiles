3aed44e9ed658050d37379ef5b8aa3975dabba56
tech1savvy <amankumar010604@gmail.com>
2025-12-15 18:18:30 +0530

    feat(task-managerment): add taskwarrior and its TUIs

--- modules/productivity.nix
+++ modules/productivity.nix
@@ -10,5 +10,11 @@
 
     # tasks
     super-productivity
+
+    taskwarrior3
+    taskwarrior-tui
+    tasksh # taskwarrior repl
+    vit # colorful and minimal
+    # tasknc # clean and minimal, but requires v2
   ];
 }
