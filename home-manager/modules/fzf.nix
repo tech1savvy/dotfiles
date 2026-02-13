4f30a66b5ddc7bb9cd8e4ca8c7c48a18c75df57f
tech1savvy <amankumar010604@gmail.com>
2026-02-13 15:32:01 +0530

    add fzf home module

---
+++ home-manager/modules/fzf.nix
@@ -0,0 +1,19 @@
+{
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    defaultOptions = [
+      "--margin=1"
+      "--layout=reverse"
+      "--border=none"
+      "--info='hidden'"
+      "--header=''"
+      "--prompt='/ '"
+      "-i"
+      "--no-bold"
+      "--bind='enter:execute(nvim {})'"
+      "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+      "--preview-window=right:60%:wrap"
+    ];
+  };
+}
