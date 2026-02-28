13e40719c3cc08326cc23d6c42a83ec85f3581e9
tech1savvy <amankumar010604@gmail.com>
2026-02-28 11:03:49 +0530

    lazygit: add gemini-powered commit message generator for lazygit

--- home-manager/modules/lazygit/default.nix
+++ home-manager/modules/lazygit/default.nix
@@ -1,8 +1,25 @@
+{ pkgs, ... }:
 {
   programs.lazygit = {
     enable = true;
 
     settings = {
+      customCommands = [
+        {
+          key = "<c-g>";
+          description = "Generate commit message via Gemini";
+          context = "global";
+          output = "terminal";
+          # TODO: Add sopsnix support
+          command = "~/bin/lazygit-gemini-commit"; # needs $GEMINI_API_KEY
+        }
+      ];
     };
   };
+
+  home.packages = with pkgs; [
+    curl
+    jq
+    gum
+  ];
 }
