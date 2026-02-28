d124c6152ba1037cb76e52e6b47b5e97ee57d17e
tech1savvy <amankumar010604@gmail.com>
2026-02-28 11:14:18 +0530

    fish: enable shell integration and add utility abbreviations for nix-helper and common pipe operations

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -1,5 +1,62 @@
 {
+  home.shell.enableFishIntegration = true;
   programs.fish = {
     enable = true;
+
+    completions = { };
+    functions = { };
+    generateCompletions = true;
+    plugins = [ ];
+    preferAbbrs = true;
+    shellAbbrs = {
+      # Nix Helper
+      ns = "nh os switch";
+      nt = "nh os test";
+      nb = "nh os build";
+      nc = "nh clean all";
+
+      P = {
+        position = "anywhere";
+        expansion = "| ";
+      };
+      # Redirect stderr to /dev/null
+      NE = {
+        position = "anywhere";
+        expansion = "2>/dev/null";
+      };
+
+      # Redirect stdout to /dev/null
+      NO = {
+        position = "anywhere";
+        expansion = ">/dev/null";
+      };
+
+      # Redirect both stdout and stderr to /dev/null
+      NUL = {
+        position = "anywhere";
+        expansion = ">/dev/null 2>&1";
+      };
+
+      # Pipe to jq
+      J = {
+        position = "anywhere";
+        expansion = "| jq";
+      };
+
+      # Copy output to clipboard (Wayland)
+      C = {
+        position = "anywhere";
+        expansion = "| wl-copy";
+      };
+    };
+    shellAliases = { };
+    binds = {
+
+    };
+
+    shellInitLast = "";
+    shellInit = "";
+    interactiveShellInit = "";
+    loginShellInit = "";
   };
 }
