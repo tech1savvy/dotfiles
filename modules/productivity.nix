0bf497e6ab68e02bd37f492c3535187566832922
tech1savvy <amankumar010604@gmail.com>
2026-02-28 07:28:51 +0530

    update(productivity)
    - add excalidraw_export utility
    - enable superproductivity unstable channel pkg
    - disable taskwarrior-tui and tasksh

--- modules/productivity.nix
+++ modules/productivity.nix
@@ -1,19 +1,22 @@
 {
   pkgs,
   ...
-}: {
+}:
+{
   environment.systemPackages = with pkgs; [
     # notes
     obsidian
+    # diagrams, spaceial documents
+    # excalidraw_export # CLI to export Excalidraw drawings to SVG and PDF
 
     # journal
 
     # tasks
-    super-productivity
+    unstable.super-productivity
 
     taskwarrior3
-    taskwarrior-tui
-    tasksh # taskwarrior repl
+    # taskwarrior-tui
+    # tasksh # taskwarrior repl
     vit # colorful and minimal
     # tasknc # clean and minimal, but requires v2
   ];
