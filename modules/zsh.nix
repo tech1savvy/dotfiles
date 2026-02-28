6520e501def22a6d3a0238678ee14263f1bfa56c
tech1savvy <amankumar010604@gmail.com>
2026-02-28 07:29:49 +0530

    feat(zsh): add system pkgs completion support

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,3 +1,7 @@
 {
   programs.zsh.enable = true;
+
+  # To get completion for system packages (e.g. systemd)
+  # required by home...zsh.enableCompletion = true;
+  environment.pathsToLink = [ "/share/zsh" ];
 }
