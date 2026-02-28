db0baf2bc3fa7642719cec5924cd8c0a4be9acbe
tech1savvy <amankumar010604@gmail.com>
2026-02-28 11:15:01 +0530

    fzf: use ripgrep for file searching and disable preview window settings

--- home-manager/modules/fzf.nix
+++ home-manager/modules/fzf.nix
@@ -2,6 +2,8 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+
+    defaultCommand = "rg --files --hidden --glob=!.git/";
     defaultOptions = [
       "--margin=1"
       "--layout=reverse"
@@ -11,9 +13,24 @@
       "--prompt='/ '"
       "-i"
       "--no-bold"
-      "--bind='enter:execute(nvim {})'"
-      "--preview='bat --style=numbers --color=always --line-range :500 {}'"
-      "--preview-window=right:60%:wrap"
+      # "--bind='enter:execute(nvim {})'"
+      # "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+      # "--preview-window=right:60%:wrap"
+    ];
+
+    fileWidgetCommand = "rg --files --hidden --glob=!.git/";
+    fileWidgetOptions = [
+      "--margin=1"
+      "--layout=reverse"
+      "--border=none"
+      "--info='hidden'"
+      "--header=''"
+      "--prompt='/ '"
+      "-i"
+      "--no-bold"
+      # "--bind='enter:execute(nvim {})'"
+      # "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+      # "--preview-window=right:60%:wrap"
     ];
   };
 }
