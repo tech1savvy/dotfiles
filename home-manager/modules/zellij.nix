bfa10b31ec2c9d4cbad5d8a1d4cf4b7aafa620d0
tech1savvy <amankumar010604@gmail.com>
2026-02-28 11:27:57 +0530

    zellij: override package with specific git revision for discovery hook and disable shell integrations

--- home-manager/modules/zellij.nix
+++ home-manager/modules/zellij.nix
@@ -1,16 +1,30 @@
+{ pkgs, ... }:
 {
   programs.zellij = {
     enable = true;
-    attachExistingSession = true;
 
-    enableZshIntegration = true;
     enableBashIntegration = false;
+    enableZshIntegration = false;
     enableFishIntegration = false;
 
-    exitShellOnExit = true;
-    extraConfig = "";
-    layouts = { };
-    settings = { };
-    themes = { };
+    attachExistingSession = false;
+    exitShellOnExit = false;
+
+    # Using this until post_command_discovery_hook becomes available
+    # source: https://github.com/zellij-org/zellij/issues/2925#issuecomment-3153295471
+    # FIX: solution did not work
+    package = pkgs.zellij.overrideAttrs (oldAttrs: rec {
+      version = "0.43.0";
+      src = oldAttrs.src.override {
+        tag = null;
+        rev = "b634a57de8f0025de36dd19e3e7916d2b27e38cd";
+        hash = "sha256-a0JJD+9neYOyDx/gAxPIdthTg695ShU93palExu3Vvo=";
+      };
+      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+        inherit src;
+        hash = "sha256-P4VabkEFBvj2YkkhXqH/JZp3m3WMKcr0qUMhdorEm1Q=";
+      };
+    });
   };
+
 }
