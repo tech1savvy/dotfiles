9a14260cce4b66d66800108dafb609596beaaa37
tech1savvy <amankumar010604@gmail.com>
2026-02-28 09:59:40 +0530

    refactor(oh-my-posh): simplify oh-my-posh configuration and update zen.toml theme
    - removed the direct file source and instead using the `configFile` attribute
    - `zen.toml` file has been updated to enclose the git branch name in parentheses `()` for better visual separation

--- home-manager/modules/oh-my-posh/default.nix
+++ home-manager/modules/oh-my-posh/default.nix
@@ -1,10 +1,10 @@
-{ lib, config, ... }:
+{ lib, ... }:
 {
-  home.file."${config.xdg.configHome}/ohmyposh/zen.toml".source = ./path/to/your/zen.toml;
   programs.oh-my-posh = {
     enable = true;
 
-    useTheme = "";
-    settings = builtins.fromTOML (lib.readFile ./oh-my-posh-zen-config.toml);
+    useTheme = null;
+    settings = { };
+    configFile = ./zen.toml;
   };
 }
