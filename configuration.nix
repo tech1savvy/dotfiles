3f7248f43f752d1dd665ffb5f32f95304ba93495
tech1savvy <amankumar010604@gmail.com>
2026-03-01 18:34:16 +0530

    chore: remove unused proxmox-nixos configuration

--- configuration.nix
+++ configuration.nix
@@ -129,9 +129,9 @@
 
   nix.settings = {
     substituters = [
-      "https://cache.saumon.network/proxmox-nixos"
+      # "https://cache.saumon.network/proxmox-nixos"
     ];
-    trusted-public-keys = [ "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM=" ];
+    # trusted-public-keys = [ "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM=" ];
   };
 
   # Do not change to maintain backwards compatibility
