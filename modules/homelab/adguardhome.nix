44b5fefdfd28b0b61424c446125c914910d4e81a
tech1savvy <amankumar010604@gmail.com>
2026-03-13 15:10:06 +0530

    adguadhome: configure adguardhome with port 533, mutable settings and local nameserver

--- modules/homelab/adguardhome.nix
+++ modules/homelab/adguardhome.nix
@@ -1,13 +1,17 @@
 { config, ... }:
 {
+  networking.nameservers = [
+    "127.0.0.1"
+  ];
+
   services.adguardhome = {
     enable = true;
 
-    port = 8080;
+    port = 533;
     host = "127.0.0.1";
     openFirewall = false;
 
     allowDHCP = config.services.adguardhome.settings.dhcp.enabled or false;
-    mutableSettings = false;
+    mutableSettings = true;
   };
 }
