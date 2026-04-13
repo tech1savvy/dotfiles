7b3fb67d55cd77220edde315e5968f8ca96afa15
tech1savvy <amankumar010604@gmail.com>
2026-04-13 16:10:24 +0530

    stylix: disable targets for hyprland, gtk, gnome, kde, and firefox

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,3 +1,10 @@
 {
-  stylix.targets.hyprland.enable = false;
+  stylix.targets = {
+    hyprland.enable = false;
+    gtk.enable = false;
+    gnome.enable = false;
+    kde.enable = false;
+    firefox.enable = false;
+  };
+
 }
