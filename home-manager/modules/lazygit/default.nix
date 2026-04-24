75fd0241b6c4daa076f40dc8d490d098052cfc1c
tech1savvy <amankumar010604@gmail.com>
2026-04-24 09:17:13 +0530

    feat(lazygit): disable warning when rewording commits with an external editor

--- home-manager/modules/lazygit/default.nix
+++ home-manager/modules/lazygit/default.nix
@@ -8,6 +8,9 @@
         # If true, switch to a different worktree without confirmation when checking out
         # a branch that is checked out in that worktree
         skipSwitchWorktreeOnCheckoutWarning = true;
+
+        # If true, do not show a warning when rewording a commit via an external editor
+        skipRewordInEditorWarning = true;
       };
 
       customCommands = [
