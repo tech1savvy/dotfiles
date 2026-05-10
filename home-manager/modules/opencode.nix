848908f941724d9cf6b3aef23bc9b2ccafe987e2
tech1savvy <amankumar010604@gmail.com>
2026-05-10 12:43:48 +0530

    opencode: add quota plugin config

--- home-manager/modules/opencode.nix
+++ home-manager/modules/opencode.nix
@@ -1,21 +1,25 @@
 { config, ... }:
 let
-  cfg = "${config.home.homeDirectory}/dotfiles/config";
+  cfg = "${config.home.homeDirectory}/dotfiles/config/opencode";
 in
 {
   home.file = {
     # OPENCODE
     ".config/opencode/opencode.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/opencode/opencode.json";
+      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/opencode.json";
     };
     # TUI
     ".config/opencode/tui.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/opencode/tui.json";
+      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/tui.json";
     };
     # PLUGINS
     # -- NOTIFIER
     ".config/opencode/opencode-notifier.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/opencode/opencode-notifier.json";
+      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/opencode-notifier.json";
+    };
+    # -- QUOTA
+    ".config/opencode/opencode-quota/quota-toast.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/opencode-quota/quota-toast.json";
     };
     # AGENTS
     ".config/opencode/agents" = {
@@ -27,9 +31,14 @@ in
       source = config.lib.file.mkOutOfStoreSymlink "${cfg}/commands";
       recursive = true;
     };
-    # COMMANDS
+    # SKILLS
     ".config/opencode/skills" = {
       source = config.lib.file.mkOutOfStoreSymlink "${cfg}/skills";
+      recursive = true;
+    };
+    # TOOLS
+    ".config/opencode/tools" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${cfg}/tools";
       recursive = true;
     };
   };
