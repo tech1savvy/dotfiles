2da5ee54ad2fbdb5e048c3c59a4a244987404066
tech1savvy <amankumar010604@gmail.com>
2026-05-20 20:51:00 +0530

    obs-studio: clean up virtual camera comment and format

--- home-manager/modules/obs-studio/default.nix
+++ home-manager/modules/obs-studio/default.nix
@@ -1,7 +1,7 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.obs-studio = {
     enable = true;
-    # enableVirtualCamera = true;
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
       obs-pipewire-audio-capture
