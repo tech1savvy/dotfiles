bab3ed9456a101fddcfd4ca827ec2618f28d4c08
tech1savvy <amankumar010604@gmail.com>
2026-05-21 08:50:27 +0530

    terminal: reorganize and add pkgs

--- modules/development/terminal.nix
+++ modules/development/terminal.nix
@@ -1,44 +1,40 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # essesntial terminal tools
-    wget # download files from the web
-
-    fzf # source: https://www.youtube.com/shorts/iUHfq_JN4G8
-
-    # json parser
-    # jq
-    jless
-
-    # comman command line tools bundle
-    busybox # tree,
-
+    # FILE MANAGEMENT
+    fff # no tui framework file browser
     # bonk # combines touch and mkdir
 
-    # modern alternatives
+    # FUN & ASCII
+    # asciiquarium
+    # cbonsai
+    # cmatrix
+    # cowsay
+    # figlet # ascii print text to terminal
+    # pipes
+
+    # JSON
+    jless
+    # jq
+
+    # NETWORKING
+    curlie # curl
+    tmate # terminal screen-sharing and access
+    wget # download files from the web
+
+    # SYSTEM UTILITIES
+    busybox # common command line tools bundle
     duf # df
     fastfetch # neofetch
-    curlie # curl
+    fzf # source: https://www.youtube.com/shorts/iUHfq_JN4G8
+    tldr # better manpages
+    # lnav # log file viewer
+    # thokr # typing test
 
+    # VIEWERS
     # glow # md file
 
     # bluetui
     # impala
-
-    # ascii art
-    # cmatrix
-    # cbonsai
-    # asciiquarium
-    # pipes
-
-    # cowsay
-
-    tldr # better manpages
-    # thokr # typing test
-
-    # lnav # log file viewer
-
-    # tmate # terminal screen-sharing and access
-    # figlet # ascii print text to terminal
   ];
 }
