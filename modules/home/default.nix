954b45357ce73cb7c7061bde4beb8e5a2ce9f60b
tech1savvy <amankumar010604@gmail.com>
2026-05-22 20:48:57 +0530

    home-manager: flatten modules directory structure

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -4,7 +4,76 @@
 }:
 {
   imports = [
-    ./modules
+    # SHELLS & PROMPT
+#     ./fish.nix
+    ./nix-your-shell.nix
+    ./shell
+    ./zsh
+    ./starship.nix
+    # ./oh-my-posh
+
+    # SHELL HISTORY & NAVIGATION
+    ./atuin.nix
+    ./fzf.nix
+    ./zoxide
+
+    # TERMINAL & MULTIPLEXERS
+    ./kitty.nix
+#     ./tmux
+    ./zellij.nix
+
+    # CLI TOOLS
+    ./bat.nix
+    ./btop.nix
+    ./eza.nix
+    ./nh.nix
+#     ./htop.nix
+    # ./glance.nix
+    ./yazi
+    ./zathura
+
+    # DEVELOPMENT
+    ./git.nix
+    ./kubecolor
+    ./lazydocker
+    ./lazygit
+    ./opencode.nix
+
+    # DESKTOP CONFIGURATION
+    ./bin.nix
+    ./stylix.nix
+    ./xdg.nix
+    # ./gtk.nix
+    # ./pointer.nix
+
+    # HYPRLAND
+    ./hyprland
+    ./hypridle
+    ./hyprlock
+    ./hyprpaper
+    ./hyprpolkitagent
+    ./hyprsunset
+    # ./hyprpanel
+
+    # DESKTOP UTILITIES
+    ./cliphist
+#     ./kdeconnect
+    ./launchers
+#     ./mpvpaper.nix
+    ./udiskie.nix
+
+    # APPLICATIONS
+#     ./activitywatch
+#     ./firefox.nix
+#     ./gemini.nix
+#     ./jrnl.nix
+#     ./keepassxc.nix
+#     ./qutebrowser
+#     ./text-expander/espanso.nix
+    # ./email-clients/mutt.nix
+    # ./element-desktop.nix
+    # ./obs-studio
+    # ./vesktop.nix
   ];
 
   home.stateVersion = "24.11"; # Presever after update as fallback if breaking changes
