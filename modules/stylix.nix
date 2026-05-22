7ec5c277c7c4fb099b9e2e74ac2c9fd368464d65
tech1savvy <amankumar010604@gmail.com>
2026-05-22 21:18:04 +0530

    stylix: switch theme from gruvbox-material to solarized-dark

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,7 +3,7 @@
   stylix = {
     enable = true;
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
 
     cursor = {
       package = pkgs.capitaine-cursors;
