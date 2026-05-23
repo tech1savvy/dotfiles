11e72c20441427ee0e0b06506a8850d3cb05f78d
tech1savvy <amankumar010604@gmail.com>
2026-05-23 16:35:35 +0530

    stylix: enable beauty-line icon theme

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -5,6 +5,13 @@
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
 
+    iconTheme = {
+      enable = true;
+      package = pkgs.beauty-line-icon-theme;
+      light = "BeautyLine";
+      dark = "BeautyLine";
+    };
+
     cursor = {
       package = pkgs.capitaine-cursors;
       name = "capitaine-cursors";
