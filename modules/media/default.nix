b895a4c66a14a8bf02d87c6d8ef56ce71f89bd05
tech1savvy <amankumar010604@gmail.com>
2026-05-24 20:46:52 +0530

    modules/media: enable pdf, image tools and video players

--- modules/media/default.nix
+++ modules/media/default.nix
@@ -1,12 +1,12 @@
 { ... }: {
   imports = [
     ./file-management.nix
-#     ./pdf.nix
-#     ./image/viewer.nix
-#     ./image/editor.nix
-#     ./video/players
-#     ./video/editor.nix
-#     ./music/players
+    ./pdf.nix
+    ./image/viewer.nix
+    ./image/editor.nix
+    ./video/players
+    # ./video/editor.nix
+    # ./music/players
     # ./video/players/ytx.nix
     # ./music/spicetify.nix
   ];
