b1347d734e03d22c1e55965a66ec6144fd830eb5
tech1savvy <amankumar010604@gmail.com>
2026-05-24 20:49:48 +0530

    easyeffects: add audio presets

--- modules/home/easyeffects/default.nix
+++ modules/home/easyeffects/default.nix
@@ -1,12 +1,31 @@
-{ config, lib, pkgs, ... }:
+# Source: https://github.com/JackHack96/EasyEffects-Presets
+let
+  advancedAutoGain = builtins.fromJSON (builtins.readFile ./presets/advanced-auto-gain.json);
+  bassBoosted = builtins.fromJSON (builtins.readFile ./presets/bass-boosted.json);
+  bassEnhancingPerfectEqLowLatency = builtins.fromJSON (
+    builtins.readFile ./presets/bass-enhancing-perfect-eq-low-latency.json
+  );
+  boosted = builtins.fromJSON (builtins.readFile ./presets/boosted.json);
+  loudnessAutogain = builtins.fromJSON (builtins.readFile ./presets/loudness-autogain.json);
+  bassEnhancingPerfectEq = builtins.fromJSON (
+    builtins.readFile ./presets/bass-enhancing-perfect-eq.json
+  );
+  perfectEq = builtins.fromJSON (builtins.readFile ./presets/perfect-eq.json);
+in
 {
   services.easyeffects = {
     enable = true;
-    # preset = "default";
-    # extraPresets = {
-    #   input = {
-    #     "my-preset.json" = { };
-    #   };
-    # };
+    preset = "";
+    extraPresets = {
+      "Advanced Auto Gain" = advancedAutoGain;
+      "Bass Boosted" = bassBoosted;
+      # Requires irs/Razor Surround ((48k Z-Edition)) 2.Stereo +20 bass.irs
+      "Bass Enhancing + Perfect EQ" = bassEnhancingPerfectEq;
+      # Requires irs/Razor Surround ((48k Z-Edition)) 2.Stereo +20 bass Low Latency.irs
+      "Bass Enhancing + Perfect EQ - Low Latency" = bassEnhancingPerfectEqLowLatency;
+      "Boosted" = boosted;
+      "Loudness+Autogain" = loudnessAutogain;
+      "Perfect EQ" = perfectEq;
+    };
   };
 }
