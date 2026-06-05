7510ed59970be608a4c16de5e68688af8a732e0c
tech1savvy <amankumar010604@gmail.com>
2026-06-05 12:13:56 +0530

    hypr: migrate monitors config to lua

---
+++ config/hypr/modules/monitors.lua
@@ -0,0 +1,8 @@
+-- For not specified quick plug and play monitors
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
+
+-- Integrated Laptop Display
+hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 2 })
+
+-- Mirror Laptop Display to HDMI Port
+hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "auto", scale = 2, mirror = "eDP-1" })
