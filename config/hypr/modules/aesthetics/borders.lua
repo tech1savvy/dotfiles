a94c1310b523ceaa5256b6570a7cef4511c3166e
tech1savvy <amankumar010604@gmail.com>
2026-06-05 12:19:01 +0530

    hypr: migrate borders config to lua

---
+++ config/hypr/modules/aesthetics/borders.lua
@@ -0,0 +1,15 @@
+hl.config({
+  general = {
+    border_size = 2,
+
+    -- Set by Stylix
+    -- ["col.active_border"] = "rgba()",
+    -- ["col.inactive_border"] = "rgba()",
+
+    -- Set to true enable resizing windows by clicking and dragging on borders and gaps
+    resize_on_border = true,
+
+    -- no_border_on_floating = false -- removed in update
+    hover_icon_on_border = false,
+  },
+})
