61392f708d13bf09d2ace40c908cc21b1e7c4793
tech1savvy <amankumar010604@gmail.com>
2026-06-06 17:35:36 +0530

    vagrant: configure firewall and networkmanager for libvirt bridges to fix vagrant dhcp

--- modules/virtualisation/vagrant/default.nix
+++ modules/virtualisation/vagrant/default.nix
@@ -1,9 +1,28 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   options.virtualisation.vagrant.enable = lib.mkEnableOption "Vagrant";
 
   config = lib.mkIf config.virtualisation.vagrant.enable {
     environment.systemPackages = [ pkgs.vagrant ];
+
+    # Unblock vagrants DHCP requests of IP addresses for created VMs
+    # Trust libvirt bridge interfaces for VM networking (required for Vagrant DHCP)
+    networking.firewall.trustedInterfaces = [
+      "virbr0"
+      "virbr1"
+      "virbr2"
+    ];
+    # Don't let NetworkManager manage libvirt bridges (prevents conflicts)
+    networking.networkmanager.unmanaged = [
+      "virbr0"
+      "virbr1"
+      "virbr2"
+    ];
   };
 }
