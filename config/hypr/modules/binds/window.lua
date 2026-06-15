7f9878d4a6a1b37309f94406de06a6a231b7c2b0
tech1savvy <amankumar010604@gmail.com>
2026-06-15 15:02:14 +0530

    hypr: swap column resize and window fullscreen keybinds

--- config/hypr/modules/binds/window.lua
+++ config/hypr/modules/binds/window.lua
@@ -4,4 +4,5 @@ hl.bind("SUPER + ALT + E", hl.dsp.exit())
 hl.bind("SUPER + ALT + V", hl.dsp.window.float({ action = "toggle" }))
 -- hl.bind("SUPER + ALT + J", hl.dsp.layout("togglesplit")) -- dwindle
 hl.bind("SUPER + P", hl.dsp.window.pin())
-hl.bind("SUPER + F", hl.dsp.window.fullscreen({ action = "toggle" }))
+-- Window Fullscreen
+hl.bind("SUPER + ALT + F", hl.dsp.window.fullscreen({ action = "toggle" }))
