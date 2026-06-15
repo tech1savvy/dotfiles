81cf16e86b597b754a47a58448ed5e052122e657
tech1savvy <amankumar010604@gmail.com>
2026-06-16 00:47:29 +0530

    dns/filter: set localhost nameserver when any dns filter is enabled

--- modules/dns/filter/default.nix
+++ modules/dns/filter/default.nix
@@ -1,6 +1,14 @@
+{ config, lib, ... }:
+with lib;
 {
   imports = [
     ./adguardhome
     ./pihole
   ];
+
+  config = mkIf (config.dns.filter.adguardhome.enable || config.dns.filter.pihole.enable || config.dns.filter.blocky.enable) {
+    networking.nameservers = [
+      "127.0.0.1"
+    ];
+  };
 }
