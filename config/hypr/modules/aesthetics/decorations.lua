b7e7d9490be41f9779b7076a0f2380a4484ce564
tech1savvy <amankumar010604@gmail.com>
2026-06-15 15:01:19 +0530

    hypr: swap active and inactive window opacity values

--- config/hypr/modules/aesthetics/decorations.lua
+++ config/hypr/modules/aesthetics/decorations.lua
@@ -3,8 +3,8 @@ hl.config({
     rounding = 0,
 
     -- Change transparency of focused and unfocused windows
-    active_opacity = 0.95,
-    inactive_opacity = 0.85,
+    active_opacity = 0.85,
+    inactive_opacity = 0.95,
     shadow = {
       enabled = true,
       range = 4,
