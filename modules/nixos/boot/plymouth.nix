f47b6d2fc2d0bed7af5a6886bd23a71c9d64206c
tech1savvy <amankumar010604@gmail.com>
2026-06-16 23:59:09 +0530

    plymouth: enable quiet boot with reduced log levels, kernel params

--- modules/nixos/boot/plymouth.nix
+++ modules/nixos/boot/plymouth.nix
@@ -1,16 +1,21 @@
-{
-  config,
-  lib,
-  ...
-}:
+{ config, lib, ... }:
 with lib;
 {
-  options.boot.splash.plymouth.enable = mkEnableOption "";
+  options.boot.splash.plymouth.enable = mkEnableOption "Plymouth graphical boot splash";
 
   config = mkIf config.boot.splash.plymouth.enable {
     boot = {
       plymouth.enable = true;
+
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "rd.udev.log_level=3"
+        "rd.systemd.show_status=auto"
+      ];
     };
+
     stylix.targets.plymouth = {
       enable = true;
       logoAnimated = true;
