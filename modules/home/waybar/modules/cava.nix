8a49eb5e4ebdcc542a206c64d7c2086fc1e1a1e3
tech1savvy <amankumar010604@gmail.com>
2026-06-22 07:46:20 +0530

    waybar(cava): enable autosens, drop manual sensitivity

--- modules/home/waybar/modules/cava.nix
+++ modules/home/waybar/modules/cava.nix
@@ -1,8 +1,7 @@
 {
   settings.cava = {
     framerate = 60;
-    autosens = 0;
-    sensitivity = 100;
+    autosens = 1;
     bars = 1;
     "lower_cutoff_freq" = 50;
     "higher_cutoff_freq" = 12000;
